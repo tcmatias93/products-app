84d70b7f4e0e89b939432bce419c5a7140a4b49f
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2025-01-07 13:22:17 -0600

    Configuracion de variables de entorno

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # Welcome to your Products App 👋
 
-This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
-
 ## Get started
 
 1. Install dependencies
@@ -10,7 +8,9 @@ This is an [Expo](https://expo.dev) project created with [`create-expo-app`](htt
    npm install
    ```
 
-2. Start the app
+2. Clonar `env.template` a `.env` y cambiar la dirección ip
+
+3. Start the app
 
    ```bash
     npx expo start
--- core/auth/api/productsApi.ts
+++ core/auth/api/productsApi.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
+import { Platform } from "react-native";
 // Conectar mediante envs vars, Android e IOS
 
+const STAGE = process.env.EXPO_PUBLIC_STAGE || "dev";
+
+export const API_URL =
+  STAGE === "prod"
+    ? process.env.EXPO_PUBLIC_API_URL
+    : Platform.OS === "ios"
+    ? process.env.EXPO_PUBLIC_API_URL_IOS
+    : process.env.EXPO_PUBLIC_API_URL_ANDROID;
+
+console.log({ STAGE, [Platform.OS]: API_URL });
 const productsApi = axios.create({
-  baseURL: "localhost:3000/api",
+  baseURL: API_URL,
 });
 
 // Interceptores
