4b0062d4cec55fb2a123118abfb25607853d6c91
Matias Torres Contreras <matiastorrescontreras@MacBook-Air-de-Matias.local>
2025-01-07 10:20:24 -0600

    Se creo estructura del proyecto de productos con login

---
+++ app/(products-app)/(home)/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { ThemedText } from "@/presentation/theme/components/ThemedText";
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <ThemedText>HomeScreen</ThemedText>
+    </View>
+  );
+};
+
+export default HomeScreen;
